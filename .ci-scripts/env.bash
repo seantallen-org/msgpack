1c74224763adbd25abaf94a051d801fe130e9fb8
Sean T. Allen <sean@seantallen.com>
2020-06-05 21:49:35 -0400

    Move from CircleCI to GitHub actions for CI

--- .ci-scripts/env.bash
+++
@@ -1,8 +0,0 @@
-REPO_OWNER="seantallen"
-REPO_NAME="pony-msgpack"
-GITHUB_USER="seantallen"
-
-# Who we are for git
-git config --global user.email "sean@seantallen.com"
-git config --global user.name "Sean T. Allen"
-git config --global push.default simple
